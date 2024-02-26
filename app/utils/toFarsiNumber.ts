e95b4fda0e8908aa92e0adf1e2d8028670a96bc2
Mohammad Khalili <EX.M.Khalili@ttcom.local>
2024-02-26 15:57:06 +0330

    add a function for convert en number to persian

---
+++ app/utils/toFarsiNumber.ts
@@ -0,0 +1,7 @@
+function toFarsiNumber(n: any) {
+  if (n) {
+    const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+    return n.toString().replace(/\d/g, (x: any) => farsiDigits[x]);
+  }
+}
+export default toFarsiNumber;
