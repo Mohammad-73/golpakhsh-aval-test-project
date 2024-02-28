0c510812db4178170e4691d7120d22dc1052d990
Mohammad Khalili <EX.M.Khalili@ttcom.local>
2024-02-28 10:52:40 +0330

    add mui font to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@ import "./globals.css";
 import Navbar from "./components/navbar/navbar";
 import { Providers } from "@/lib/redux/provider";
 import { ToastContainer } from "react-toastify";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
